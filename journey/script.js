64ca4b064e69826e102373076a18a86fad317b1f
Brad <128764758+piigoo@users.noreply.github.com>
2023-10-20 16:36:07 +0800

    Add files via upload

---
+++ journey/script.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function() {
+ 
+  jQuery(".owl-carousel").owlCarousel({ 
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+      autoPlay : true
+  });
+ 
+});
