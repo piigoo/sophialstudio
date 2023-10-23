62a41a3c7a586809db0a40992df0c1b845e36387
Brad <128764758+piigoo@users.noreply.github.com>
2023-10-23 11:22:23 +0800

    Update script.js

--- 3d_modeling/script.js
+++ 3d_modeling/script.js
@@ -1,15 +1,13 @@
-import { faker } from "https://cdn.skypack.dev/@faker-js/faker@7.6.0";
-
 // 图片后缀数组
 const imageExtensions = [
-'project_6_1.jpg',
-'project_6_2.png',
-'project_6_3.png',
-'project_6_4.jpg',
-'project_6_5.jpg',
-'project_6_6.jpg',
-'project_6_7.jpg'];
-
+  'project_6_1.jpg',
+  'project_6_2.png',
+  'project_6_3.png',
+  'project_6_4.jpg',
+  'project_6_5.jpg',
+  'project_6_6.jpg',
+  'project_6_7.jpg'
+];
 
 const SPEAKERS = document.querySelectorAll('li');
 SPEAKERS.forEach((SPEAKER, index) => {
@@ -17,12 +15,16 @@ SPEAKERS.forEach((SPEAKER, index) => {
   const details = SPEAKER.querySelector('.speaker__details');
   const name = SPEAKER.querySelector('.speaker__name');
   const title = SPEAKER.querySelector('.speaker__title');
-  const fullName = `${faker.name.firstName()} ${faker.name.lastName()}`;
-  const job = `${faker.name.jobTitle()}, ${faker.company.name()}.`;
+  
+  // 设置为固定的名字、职位和细节
+  const fullName = "Sophia Liu";
+  const job = "Designer";
+  const detailText = "3D_modeling_practice";
+  
   name.innerText = fullName;
   title.innerText = job;
-  details.innerText = `${fullName}. ${job}`;
-
+  details.innerText = `${fullName}. ${detailText}`;
+  
   // 使用数组中的对应图片后缀
   img.src = `https://sophial.studio/images/album/${imageExtensions[index]}?format=auto`;
   img.alt = `Image of ${fullName}`;
